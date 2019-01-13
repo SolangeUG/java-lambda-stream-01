7192ac0f3a03bce61e4e684a64ba900d4232cb1b
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2019-01-13 20:02:56 +0000

    Git repository initialization and project setup

---
+++ src/main/java/org/paumard/lambdamasterclass/part1/util/CurrencyConverter.java
@@ -0,0 +1,7 @@
+package org.paumard.lambdamasterclass.part1.util;
+
+public interface CurrencyConverter {
+
+    double convert(double amount);
+
+}
---
+++ src/main/java/org/paumard/lambdamasterclass/part1/util/Validator.java
@@ -0,0 +1,5 @@
+package org.paumard.lambdamasterclass.part1.util;
+
+public interface Validator<T> {
+
+}
